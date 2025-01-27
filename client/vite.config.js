2ca99ce4c21c005195f58e8b5ae1d4d6e3226167
barisyazici_ <C9014100@arcelik.com>
2025-01-27 19:59:52 +0300

    client düzenlendi.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
@@ -15,5 +15,12 @@ export default defineConfig({
         target: 'http://localhost:3002'
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+    emptyOutDir: true
   }
 }); 
